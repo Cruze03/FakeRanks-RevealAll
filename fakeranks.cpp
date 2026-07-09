a8b9b6de4dc50f67896ceb7782281dffd3c3496d
Cruze03 <sachinfnatic@gmail.com>
2026-07-09 23:00:24 +0530

    fix: game update 09/07

--- fakeranks.cpp
+++ fakeranks.cpp
@@ -162,7 +162,7 @@ const char *FakeRank_RevealAll::GetLicense()
 
 const char *FakeRank_RevealAll::GetVersion()
 {
-	return "1.1.1";
+	return "1.1.2";
 }
 
 const char *FakeRank_RevealAll::GetDate()
